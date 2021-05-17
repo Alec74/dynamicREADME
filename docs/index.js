b8d64a3aa15d07e229b7aa4ce495e78dccddf66c
Alec74 <alecmcglone@gmail.com>
2021-05-17 19:19:47 -0400

    inquirer added

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,12 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = ['What was your build motivation?', 'Why did you build this project?',
-    'What problem does it solve?', 'What makes your project standout?'];
+const questions = ['What is the title of your project?', 'What was your build motivation?', 'Why did you build this project?',
+    'What problem does it solve?', 'What makes your project standout?', 'What are the steps to install your project?',
+    'Please provide instructions for usage.'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -14,9 +17,55 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log('Welcome! Your answers following questions will be added to your new README!');
-
-}
+    // console.log('Welcome! Your answers following questions will be added to your new README file!');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'motiv',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'why',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'problem',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'standout',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'install',
+            },
+            {
+                type: 'input',
+                message: questions[6],
+                name: 'usage',
+            },
+        ])
+        .then((response) => {
+            console.log(response.title);
+            console.log(response.motiv);
+            console.log(response.why);
+            console.log(response.problem);
+            console.log(response.standout);
+            console.log(response.install);
+            console.log(response.usage);
+        });
+};
 
 // Function call to initialize app
 init();
